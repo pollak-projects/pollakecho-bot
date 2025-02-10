165f130b1310e0531f444f1b3973c19e5c12d6d5
FaboRobi <fabodudasr@gmail.com>
2025-02-10 12:52:29 +0100

    Bot Struktura

--- bot.js
+++ bot.js
@@ -1,11 +1,13 @@
-
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config()
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	require('./handler/commandHandler.js');
+	require('./handler/eventHandler.js')
 });
 
 client.login(process.env.TOKEN);
+
+module.exports = {client}
