a2389792567ba1b5a67bfca52ee96a4166a6c5ff
FaboRobi <fabodudasr@gmail.com>
2025-02-14 09:36:33 +0100

    Magyarosítás

--- commands/uzenet/uzenet.js
+++ commands/uzenet/uzenet.js
@@ -2,16 +2,16 @@ const { SlashCommandBuilder } = require('discord.js');
 
 const data = new SlashCommandBuilder()
     .setName('uzenet')
-    .setDescription('Replies with your input!')
+    .setDescription('Válaszol az Inputoddal')
     .addStringOption(option =>
         option.setName('input')
-            .setDescription('The input to echo back')
+            .setDescription('Elküldi az inputot')
             .setRequired(true));
 
 module.exports = {
     data: data,
     execute: async (interaction) => {
         const input = interaction.options.getString('input');
-        await interaction.reply(`You said: ${input}`);
+        await interaction.reply(`${input}`);
     },
 };
