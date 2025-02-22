5e54bf2d3cf1002b2a88f731f7e0c41857a8156d
Feke András <afeke@pollak.hu>
2025-02-22 23:52:28 +0100

    Refactor top command to use EmbedBuilder instead of MessageEmbed and update field formatting

--- commands/top/top.js
+++ commands/top/top.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
-const { MessageEmbed } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -25,25 +25,16 @@ module.exports = {
         throw new Error(`Hiba történt: ${response.statusText}`);
       }
       const data = await response.json();
-
-      /*  [ {
-         "userId": "1a3aa7c9-121c-490c-ba1f-c4a0d5937289",
-        "discordId": "298806399050317826",
-        "point": 0,
-        "name": "Huszka Adrián Gábor"
-        }]*/
-
-      const embed = new MessageEmbed()
+      const embed = new EmbedBuilder()
         .setTitle(`Top 10 felhasználó a(z) ${channel.name} csatornán`)
         .setColor("#0099ff")
         .setTimestamp();
 
       data.forEach((user, index) => {
-        embed.addField(
-          `#${index + 1} ${user.name}`,
-          `Pontszám: ${user.point}`,
-          `Discord ID: @${user.discordId}`
-        );
+        embed.addFields({
+          name: `#${index + 1} ${user.name}`,
+          value: `Pontszám: ${user.point}\nDiscord ID: @${user.discordId}`,
+        });
       });
 
       await interaction.reply({ embeds: [embed] });
