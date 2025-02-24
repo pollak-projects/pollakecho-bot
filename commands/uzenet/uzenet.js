f5ee450fe395bbe56dc04b40e2c7360dfd18c8d5
FaboRobi <fabodudasr@gmail.com>
2025-02-24 13:17:45 +0100

    uzenet javitas

--- commands/uzenet/uzenet.js
+++ commands/uzenet/uzenet.js
@@ -1,35 +1,35 @@
 const { SlashCommandBuilder } = require('discord.js');
-
-data: new SlashCommandBuilder()
-.setName("uzenet")
-.setDescription(
-  "Továbbít egy üzenetet a tanártól a diákok felé"
-)
-.addChannelOption((option) =>
-  option
-    .setName("csatorna")
-    .setDescription(
-      "A felhasználó megadja a csatornát, amelyen a tanár üzenetét meg szeretné jeleníteni"
-    )
-    .setRequired(true)
-).addStringOption((option) =>
-  option
-    .setName("oktato")
-    .setDescription(
-      "A felhasználó megadja a tanár nevét, aki az üzenetet küldi"
-    )
-    .setRequired(true)
-).addStringOption((option) =>
-  option
-    .setName("uzenet")
-    .setDescription(
-      "A felhasználó megadja az üzenetet, amelyet a tanár a diákoknak küld"
-    )
-    .setRequired(true)
-),
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    data: data,
+  data: new SlashCommandBuilder()
+  .setName("uzenet")
+  .setDescription(
+    "Továbbít egy üzenetet a tanártól a diákok felé"
+  )
+  .addChannelOption((option) =>
+    option
+      .setName("csatorna")
+      .setDescription(
+        "A felhasználó megadja a csatornát, amelyen a tanár üzenetét meg szeretné jeleníteni"
+      )
+      .setRequired(true)
+  ).addStringOption((option) =>
+    option
+      .setName("oktato")
+      .setDescription(
+        "A felhasználó megadja a tanár nevét, aki az üzenetet küldi"
+      )
+      .setRequired(true)
+  ).addStringOption((option) =>
+    option
+      .setName("uzenet")
+      .setDescription(
+        "A felhasználó megadja az üzenetet, amelyet a tanár a diákoknak küld"
+      )
+      .setRequired(true)
+  ),
+
     execute: async (interaction) => {
         const channel = interaction.options.getChannel("csatorna");
         const teacher = interaction.options.getString("oktato");
@@ -45,7 +45,6 @@ module.exports = {
         embed.addFields({
             name: `${message}`,
         });
-
 
         await interaction.client.channels.cache.get(channel.id).send({
         embeds: [embed],
