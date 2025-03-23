5cf4bbaff2410babe3d20bc55cb5fdb9cc74a655
Feke András <afeke@pollak.hu>
2025-03-23 15:51:38 +0100

    Add additional mappings for szakma and agazat dictionaries in link command

--- commands/link/link.js
+++ commands/link/link.js
@@ -2,11 +2,16 @@ const { SlashCommandBuilder, MessageFlags } = require("discord.js");
 require("dotenv").config();
 
 const szakmaDictionary = {
+  "eeecd4ab-5a1a-4a1e-a8ef-28408c3df97a": "1353379465622978580",
+  "b1a33d92-c5ce-4f2a-abad-e8a55c3fc849": "1347466557554692096",
   "71b9c9c6-71e0-4bc1-8375-8f586608a869": "1347466691017183262",
+  "67b1f569-3d26-41b5-8563-75a556d0e2c9": "1347466849184387153",
+  "52879ae6-16a5-4ca9-b8c7-a366f5607000": "1347466755991273523",
 };
 
 const agazatDictionary = {
   "7278f3ab-b9a4-40f2-a794-e51cee8487f9": "1347466691017183262",
+  "8f592440-5a10-4eb6-9465-066c2f14815b": "1347466849184387153",
 };
 
 const evfolyamDictionary = {
@@ -15,6 +20,7 @@ const evfolyamDictionary = {
   11: "1336625881929482321",
   12: "1336625977073205279",
   13: "1336625996132126741",
+  "1/13": "1336625996132126741",
 };
 
 const giveRoleBasedOnDictionarys = async (interaction, data) => {
